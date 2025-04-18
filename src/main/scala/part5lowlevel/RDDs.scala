d8537f60b2b4b81b4fa437dd8acac2008cf8c04e
Carlos Pov <carlospovedat1388@gmail.com>
2025-04-18 12:12:03 +0200

    wip: rdds lesson 1

---
+++ src/main/scala/part5lowlevel/RDDs.scala
@@ -0,0 +1,41 @@
+package part5lowlevel
+
+import org.apache.spark.sql.SparkSession
+
+import scala.io.Source
+
+object RDDs extends App{
+  val spark = SparkSession.builder()
+    .appName("Introduction to RDDs")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val sc = spark.sparkContext // to operate with RDDs
+
+  // Creating RDDs
+  // 1 - paralellize an existing collection
+  val numbers = 1 to 100000
+  val numbersRDD = sc.parallelize(numbers)
+
+  // 2 - reading from files
+  case class StockValue(company: String, date: String, price: Double)
+  def readStocks(filename: String) =
+    Source.fromFile(filename)
+      .getLines()
+      .drop(1) // to drop header
+      .map(line => line.split(","))
+      .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+      .toList
+
+  val stocksRDD = sc.parallelize(readStocks("src/main/resources/data/stocks.csv"))
+
+  // 2b - reading from files
+  val stocksRDD2 = sc.textFile("src/main/resources/data/stocks.csv")
+    .map(line => line.split(","))
+    .filter(tokens => tokens(0).toUpperCase() == tokens(0)) // to filter header (knowing how the header is, in this case we know is lowercase)
+    .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+
+  // 3 - read from a DataFrame
+
+
+}
